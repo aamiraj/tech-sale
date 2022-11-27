ed6b660e46e515464cbda5c1839c08d40d8ddff2
Abdullah Miraj <miraj2465@gmail.com>
2022-11-27 22:09:06 +0600

    updated my product page with delete button and used tanstack query

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import UserContext from "./contexts/UserContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <UserContext>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </UserContext>
   </React.StrictMode>
 );
--- src/pages/MyProducts.js
+++ src/pages/MyProducts.js
@@ -1,19 +1,39 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { AuthContext } from "../contexts/UserContext";
+import { useQuery } from "@tanstack/react-query";
+import toast, { Toaster } from "react-hot-toast";
 
 function MyProducts() {
   const { user } = useContext(AuthContext);
-  const [userlaptops, setUserLaptops] = useState([]);
 
-  useEffect(() => {
-    fetch(`http://localhost:5000/products?email=${user.email}`)
+  const { data: userlaptops = [], refetch } = useQuery({
+    queryKey: ["userlaptops"],
+    queryFn: async () => {
+      const res = await fetch(
+        `http://localhost:5000/products?email=${user.email}`
+      );
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  const handleDelete = (id) => {
+    fetch(`http://localhost:5000/products?id=${id}`, {
+      method: "DELETE",
+    })
       .then((res) => res.json())
-      .then((data) => setUserLaptops(data));
-  }, [user]);
+      .then((data) => {
+        if (data.deletedCount > 0) {
+          toast.success("Product delete successful");
+          refetch();
+        }
+      });
+  };
 
   return (
     <div>
       <div className="overflow-x-auto w-full">
+      <Toaster position="top-center" reverseOrder={false}></Toaster>
         <table className="table w-full">
           <thead>
             <tr>
@@ -26,18 +46,22 @@ function MyProducts() {
           </thead>
           <tbody>
             {userlaptops.map((laptop, i) => (
-                <tr key={i}>
-                  <td>{laptop.product}</td>
-                  <td>{laptop.resalePrice}</td>
-                  <td>{laptop.sold ? "Sold" : "Unsold"}</td>
-                  <td>
-                    <button className="btn btn-error btn-xs">Delete</button>
-                  </td>
-                  <td>
-                    <button className="btn btn-info btn-xs">Advertise</button>
-                  </td>
-                </tr>
-              
+              <tr key={i}>
+                <td>{laptop.product}</td>
+                <td>{laptop.resalePrice}</td>
+                <td>{laptop.sold ? "Sold" : "Unsold"}</td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(laptop._id)}
+                    className="btn btn-error btn-xs"
+                  >
+                    Delete
+                  </button>
+                </td>
+                <td>
+                  <button className="btn btn-info btn-xs">Advertise</button>
+                </td>
+              </tr>
             ))}
           </tbody>
         </table>
